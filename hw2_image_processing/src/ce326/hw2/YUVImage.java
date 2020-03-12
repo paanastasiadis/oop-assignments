38a6c34155623b21cfa44b0fb5db8de4cb344b5f
Panagiotis Anastasiadis <panag1234@outlook.com>
2020-03-12 18:01:40 +0200

    Add classes for operations on YUV Images & Pixels

---
+++ hw2_image_processing/src/ce326/hw2/YUVImage.java
@@ -0,0 +1,162 @@
+package ce326.hw2;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class YUVImage {
+    private YUVPixel[][] image;
+    private int imageWidth;
+    private int imageHeight;
+
+    public YUVImage(int width, int height) {
+        setDefaultImage(width, height);
+    }
+
+    public YUVImage(YUVImage copyImg) {
+        setImage(copyImg.getHeight(), copyImg.getWidth(), copyImg.image);
+    }
+
+    public YUVImage(RGBImage RGBImg) {
+        this.imageHeight = RGBImg.getHeight();
+        this.imageWidth = RGBImg.getWidth();
+        this.image = new YUVPixel[this.getHeight()][this.getWidth()];
+        for (int i = 0; i < this.getHeight(); i++) {
+            for (int j = 0; j < this.getWidth(); j++) {
+                setPixel(i, j, new YUVPixel(RGBImg.getPixel(i, j)));
+            }
+        }
+    }
+
+    public YUVImage(java.io.File file) throws FileNotFoundException, UnsupportedFileFormatException {
+
+        Scanner sc = new Scanner(file);
+
+        if (!isYUVExtension(file)) {
+            throw new UnsupportedFileFormatException(file.getName());
+        }
+
+        sc.next();
+
+        int yuvWidth = sc.nextInt();
+        int yuvHeight = sc.nextInt();
+
+        this.setImage(yuvWidth, yuvHeight);
+
+        int i = 0;
+        int j = 0;
+        while (sc.hasNext()) {
+            short Y = sc.nextShort();
+            short U = sc.nextShort();
+            short V = sc.nextShort();
+            this.setPixel(i, j, new YUVPixel(Y, U, V));
+
+            if (j == this.getWidth() - 1) {
+                j = 0;
+                i++;
+            } else {
+                j++;
+            }
+        }
+    }
+
+    //TODO See if you can extract this method as universal for PPM and YUV extensions
+    private boolean isYUVExtension(java.io.File file) {
+        if (file == null) {
+            return false;
+        }
+        String name = file.getName();
+        int i = name.lastIndexOf('.');
+        if (i > 0) {
+            return name.substring(i + 1).equals("yuv");
+        }
+        return false;
+    }
+
+    public void setDefaultImage(int width, int height) {
+        this.imageWidth = width;
+        this.imageHeight = height;
+        this.image = new YUVPixel[height][width];
+        YUVPixel defaultPixel = new YUVPixel((short) 16, (short) 128, (short) 128);
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                setPixel(i, j, defaultPixel);
+            }
+        }
+    }
+
+    public void setImage(int width, int height) {
+        this.imageHeight = height;
+        this.imageWidth = width;
+        this.image = new YUVPixel[height][width];
+
+    }
+
+    public void setImage(int width, int height, YUVPixel[][] pixelArray) {
+        this.imageHeight = height;
+        this.imageWidth = width;
+        this.image = pixelArray;
+    }
+
+    public int getHeight() {
+        return this.imageHeight;
+    }
+
+    public int getWidth() {
+        return this.imageWidth;
+    }
+
+    public void setPixel(int row, int col, YUVPixel pixelYUVPixel) {
+        this.image[row][col] = pixelYUVPixel;
+    }
+
+    public YUVPixel getPixel(int row, int col) {
+        return this.image[row][col];
+    }
+
+    public String toString() {
+        StringBuilder YUVStr = new StringBuilder();
+        YUVStr.append("YUV3\n");
+        YUVStr.append(this.getWidth());
+        YUVStr.append(" ");
+        YUVStr.append(this.getHeight());
+        YUVStr.append("\n");
+
+        for (int i = 0; i < this.getHeight(); i++) {
+            for (int j = 0; j < this.getWidth(); j++) {
+                YUVStr.append(this.getPixel(i, j).toString());
+                YUVStr.append("\n");
+            }
+        }
+        return YUVStr.toString();
+    }
+
+    //TODO Do something for the duplication between this toFile and PPM's
+    public void toFile(java.io.File file) {
+        //TODO Search for alternative method of truncation
+        if (file.exists() && !file.isDirectory()) {
+            file.delete();
+        }
+        //TODO Search for optimal way of writing to file
+        try (PrintWriter outHistogram = new PrintWriter(file)) {
+            outHistogram.print(this.toString());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void equalize() {
+//        YUVImage equalizedImage = new YUVImage(this);
+
+        Histogram imgHistogram = new Histogram(this);
+        imgHistogram.equalize();
+
+        for (int i = 0; i < this.getHeight(); i++) {
+            for (int j = 0; j < this.getWidth(); j++) {
+                short oldYValue = this.getPixel(i, j).getY();
+                this.getPixel(i, j).setY(imgHistogram.getEqualizedLuminosity(oldYValue));
+            }
+        }
+    }
+}
